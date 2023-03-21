b5d37e91efe61f30dc46c865287234d7172c3ba7
Isanin Sergey <sergey.isanin@gmail.com>
2023-03-21 16:02:26 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/components/HomePage.vue';
 import AboutPage from '@/components/AboutPage.vue';
 import ErrorPage from '@/components/ErrorPage.vue';
@@ -10,7 +10,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
