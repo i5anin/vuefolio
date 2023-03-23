428e61cea38eea004c08650d7a4b1cf1ae13c395
Isanin Sergey <sergey.isanin@gmail.com>
2023-03-23 18:55:26 +0300

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-  import FooterPage from './components/FooterPage.vue';
+  import FooterPage from '@/vue/components/FooterPage.vue';
 
   export default {
     components: {
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '@/components/HomePage.vue';
-import AboutPage from '@/components/AboutPage.vue';
-import TabTest from '@/components/TabTest.vue';
-import ErrorPage from '@/components/ErrorPage.vue';
+import HomePage from '@/vue/pages/HomePage.vue';
+import AboutPage from '@/vue/pages/AboutPage.vue';
+// import TabTest from '@/pages/TabTest.vue';
+import ErrorPage from '@/vue/pages/ErrorPage.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
   { path: '/about', name: 'About', component: AboutPage },
-  { path: '/test', name: 'Test', component: TabTest },
+  // { path: '/test', name: 'Test', component: TabTest },
   { path: '/:pathMatch(.*)', name: 'Error404', component: ErrorPage },
 ];
 
