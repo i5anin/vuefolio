f46eb52e702c96238d38b5951f6060edc9029b40
Isanin Sergey <sergey.isanin@gmail.com>
2023-03-23 23:27:26 +0300

    Yandex.Metrika

--- src/vue/pages/HomePage.vue
+++ src/vue/pages/HomePage.vue
@@ -4,6 +4,7 @@
       <p class="title">Hello! I am</p>
       <h1 class="title">Sergey Isanin</h1>
       <h2>Junior front-end developer</h2>
+      <h2>I want to be a middle front-end developer</h2>
 
       <p>// write the code below to continue</p>
       <p class="color">> <span class="underline">w</span>hois</p>
