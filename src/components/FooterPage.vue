d09c34677bfc79367977ea5e27b04a2eadf83d4c
Isanin Sergey <sergey.isanin@gmail.com>
2023-03-23 15:13:42 +0300

    bootstrap-icons

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -4,6 +4,12 @@
 
     <!-- New footer bar -->
     <div class="footer">
+      <div>
+        <i class="bi bi-house-fill"></i>
+        <i class="bi bi-envelope-fill"></i>
+        <i class="bi bi-youtube"></i>
+      </div>
+      <div class="divider"></div>
       <p>{{ currentTime }}</p>
     </div>
   </div>
@@ -24,3 +30,37 @@
     },
   };
 </script>
+
+<style scoped>
+  .bi {
+    padding: 5px 10px;
+  }
+  .footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background-color: #000000;
+    border-top: 1px solid #e7e7e7;
+    padding: 10px 0;
+    text-align: center;
+    height: 50px;
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center; /* added to vertically center the content */
+  }
+
+  .footer p {
+    margin-bottom: 0;
+    margin-right: 15px;
+  }
+
+  .footer .divider {
+    height: 100%;
+    border-left: 1px solid #e7e7e7; /* added to create a vertical line */
+    margin-left: 15px; /* added to create an indent */
+    margin-right: 15px; /* added to create a gap between the line and the icons */
+    flex-shrink: 0; /* added to prevent the element from shrinking */
+  }
+</style>
