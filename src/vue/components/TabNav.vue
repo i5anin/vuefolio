3d4c4b46b4b17db90142b8b89e45a3daa7360cb0
Isanin Sergey <sergey.isanin@gmail.com>
2023-03-27 00:34:03 +0300

    tab style

--- src/vue/components/TabNav.vue
+++ src/vue/components/TabNav.vue
@@ -1,16 +1,17 @@
 <template>
   <b-tabs v-model="activeTab">
     <router-link
-      class="tab"
+      class="tab-container"
       v-for="(tab, index) in tabs"
       :key="index"
       :to="tabRoutes[index]"
       :title="tab">
-      <b-tab :class="{ active: activeTab === index }">
+      <b-tab
+        :class="{ active: activeTab === index, inactive: activeTab !== index }">
         {{ tab }}
       </b-tab>
     </router-link>
-    <nav class="tab-nav-container"></nav>
+    <!-- <nav class="tab-nav-container"></nav> -->
   </b-tabs>
 </template>
 
@@ -40,23 +41,47 @@
 </script>
 
 <style lang="css">
-  .tab {
+  .tab-container {
     font-size: 20px;
-    outline: 1px solid #828282;
+    /* border-top: 1px solid #828282;
+    border-right: 1px solid #828282;
+    border-left: 1px solid #828282;
+    border-bottom: 1px solid #828282; */
     text-decoration: none;
-    padding: 10px 80px 10px 70px;
-    color: #828282;
+    /* padding: 10px 80px 10px 70px; */
+    /* color: #828282; */
+    /* background-color: #202020; */
   }
   .active {
     color: rgb(255, 140, 0);
     font-weight: bold;
-  }
-  .tab-nav-container {
-    padding: 6px;
-    position: relative;
+
+    border-top: 3px solid #ff0000;
+    border-right: 1px solid #828282;
+    border-left: 1px solid #828282;
+    border-bottom: 0px solid #828282;
+
+    padding: 10px 80px 10px 70px;
   }
 
-  .tab-nav-container::before {
+  .inactive {
+    /* color: #828282; */
+    font-weight: bold;
+
+    border-top: 1px solid #828282;
+    border-right: 1px solid #828282;
+    border-left: 1px solid #828282;
+    border-bottom: 1px solid #828282;
+
+    padding: 10px 80px 10px 70px;
+  }
+
+  /* .tab-nav-container {
+    padding: 6px;
+    position: relative;
+  } */
+
+  /* .tab-nav-container::before {
     content: '';
     position: absolute;
     bottom: 0;
@@ -64,5 +89,5 @@
     width: 100%;
     height: 1px;
     background-color: #828282;
-  }
+  } */
 </style>
