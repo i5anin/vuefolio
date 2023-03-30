a85d55b0abf3cae1994cc69565711cdb5e33fb65
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-03-30 22:11:11 +0300

    +

--- src/vue/components/TabNav.vue
+++ src/vue/components/TabNav.vue
@@ -1,14 +1,15 @@
 <template>
-  <v-tabs v-model="activeTab" color="primary" align-with-title>
-    <v-tab v-for="(tab, index) in tabs" :key="index">{{ tab.name }}</v-tab>
+  <v-tabs v-model="activeTab" color="primary" align-with-title ref="tabs">
+    <v-tab v-for="(tab, index) in localTabs" :key="index">{{ tab.name }}</v-tab>
     <v-tabs-items v-model="activeTab">
-      <v-tab-item v-for="(tab, index) in tabs" :key="index">
+      <v-tab-item v-for="(tab, index) in localTabs" :key="index">
         <v-card>
           <v-card-title>
             <h2>{{ tab.name }}</h2>
           </v-card-title>
           <v-card-text>
-            <component :is="tab.component"></component>
+            <component v-if="tab.component" :is="tab.component"></component>
+            <div v-else>Компонент не найден</div>
           </v-card-text>
         </v-card>
       </v-tab-item>
@@ -29,6 +30,19 @@
       return {
         activeTab: 0,
       };
+    },
+    computed: {
+      localTabs() {
+        return this.tabs.slice();
+      },
+    },
+    mounted() {
+      const items = this.$refs.tabs.items;
+      items.forEach((item, index) => {
+        item.$el.addEventListener('click', () => {
+          this.activeTab = index;
+        });
+      });
     },
   };
 </script>
