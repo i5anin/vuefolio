cd49320535cf56538864f05858cf1a10ff21b67d
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-03-30 22:15:05 +0300

    Update TabNav.vue

--- src/vue/components/TabNav.vue
+++ src/vue/components/TabNav.vue
@@ -1,47 +1,31 @@
 <template>
-  <v-tabs
-    v-model="activeTab"
-    background-color="transparent"
-    slider-color="#333"
-    height="100%"
-    width="100%"
-    v-if="localTabs.length > 0">
-    <v-tab
-      v-for="(tab, index) in localTabs"
-      :key="index"
-      :class="activeTab === tab.id ? 'primary' : ''"
-      @click="selectTab(tab)">
-      {{ tab.label }}
-    </v-tab>
-  </v-tabs>
+  <div>
+    <TabNav :tabs="tabs" @tab-selected="onTabSelected" />
+    <!-- остальной код -->
+  </div>
 </template>
 
 <script>
+  import TabNav from './components/TabNav.vue';
+
   export default {
-    name: 'TabNav',
-    props: {
-      tabs: {
-        type: Array,
-        default: () => [],
-      },
+    name: 'App',
+    components: {
+      TabNav,
     },
     data() {
       return {
-        activeTab: '',
+        tabs: [
+          { id: 'home', label: 'Home' },
+          { id: 'about', label: 'About' },
+          { id: 'contact', label: 'Contact' },
+        ],
+        activeTab: 'home',
       };
     },
-    computed: {
-      localTabs() {
-        if (this.tabs.length === 0) {
-          return [];
-        }
-        return this.tabs.slice();
-      },
-    },
     methods: {
-      selectTab(tab) {
-        this.activeTab = tab.id;
-        this.$emit('tab-selected', tab.id);
+      onTabSelected(tabId) {
+        this.activeTab = tabId;
       },
     },
   };
