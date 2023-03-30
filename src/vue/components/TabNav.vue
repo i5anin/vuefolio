db1308876d3d2a8aafce719d56006a46b8be4fb2
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-03-30 22:08:01 +0300

    test

--- src/vue/components/TabNav.vue
+++ src/vue/components/TabNav.vue
@@ -1,66 +1,34 @@
 <template>
-  <b-tabs v-model="activeTab">
-    <router-link
-      class="tab-container"
-      v-for="(tab, index) in tabs"
-      :key="index"
-      :to="tabRoutes[index]"
-      :title="tab">
-      <b-tab
-        :class="{ active: activeTab === index, inactive: activeTab !== index }">
-        {{ tab }}
-      </b-tab>
-    </router-link>
-    <!-- <nav class="tab-nav-container"></nav> -->
-  </b-tabs>
+  <v-tabs v-model="activeTab" color="primary" align-with-title>
+    <v-tab v-for="(tab, index) in tabs" :key="index">{{ tab.name }}</v-tab>
+    <v-tabs-items v-model="activeTab">
+      <v-tab-item v-for="(tab, index) in tabs" :key="index">
+        <v-card>
+          <v-card-title>
+            <h2>{{ tab.name }}</h2>
+          </v-card-title>
+          <v-card-text>
+            <component :is="tab.component"></component>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-tabs>
 </template>
 
 <script>
   export default {
     name: 'TabNav',
+    props: {
+      tabs: {
+        type: Array,
+        required: true,
+      },
+    },
     data() {
       return {
-        tabs: ['_hello', '_about me', '_projects'],
-        tabRoutes: ['/', '/about', '/projects'],
         activeTab: 0,
       };
     },
-    mounted() {
-      const currentRoute = this.$router.currentRoute.path;
-      this.activeTab = this.tabRoutes.indexOf(currentRoute);
-    },
-    watch: {
-      $route(to) {
-        const index = this.tabRoutes.indexOf(to.path);
-        if (index >= 0) {
-          this.activeTab = index;
-        }
-      },
-    },
   };
 </script>
-
-<style lang="css">
-  .tab-container {
-    font-size: 20px;
-    text-decoration: none;
-  }
-  .active {
-    color: rgb(255, 140, 0);
-    border-top: 3px solid #ff0000;
-    border-right: 1px solid #828282;
-    border-left: 1px solid #828282;
-    border-bottom: 0px solid #828282;
-    font-weight: bold;
-    padding: 10px 80px 10px 70px;
-  }
-
-  .inactive {
-    border-top: 1px solid #828282;
-    border-right: 1px solid #828282;
-    border-left: 1px solid #828282;
-    border-bottom: 1px solid #828282;
-    font-weight: bold;
-    padding: 10px 80px 10px 70px;
-  }
-</style>
