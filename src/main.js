5e2e8c4b2fd922a903f54bc367e8d44645f54c09
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-04-01 21:10:46 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,11 @@ import App from './App.vue';
 import router from './router';
 import { createVuetify } from 'vuetify';
 import 'vuetify/dist/vuetify.css';
-import '@mdi/font/css/materialdesignicons.css';
+
+// import '@mdi/font/css/materialdesignicons.css';
 
 const app = createApp(App);
-app.use(router);
-
 const vuetify = createVuetify();
-
+app.use(router);
 app.use(vuetify);
-
 app.mount('#app');
