f61fcaab98e746eaba06fd36c958b35786a7292a
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-04-01 20:38:07 +0300

    Update AboutPage.vue

--- src/vue/pages/AboutPage.vue
+++ src/vue/pages/AboutPage.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="about-us">
     <v-container>
+      <div class="container">
+        <h1 class="title">О нас</h1>
+        <div class="subtitle">
+          <p>
+            Мы занимаемся созданием качественных и современных веб-приложений.
+          </p>
+          <p>
+            В нашей команде вы найдете высококвалифицированных специалистов по
+            разработке программного обеспечения, дизайнеров и маркетологов.
+          </p>
+        </div>
+        <h2 class="subtitle">Технологии</h2>
+        <div class="content">
+          <div class="columns">
+            <div class="column" v-for="tech in techStack" :key="tech.name">
+              <div class="card">
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">{{ tech.name }}</p>
+                    </div>
+                  </div>
+                  <div class="content">
+                    <p>{{ tech.description }}</p>
+                    <p>
+                      <a :href="tech.link">{{ tech.link }}</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <v-row>
         <v-col cols="12" md="6">
           <h1 class="display-1 mb-4">About Us</h1>
@@ -27,8 +62,20 @@
 </template>
 
 <script>
+  import techStackData from '@/data/tech-stack.json';
+  import { VRow, VCol } from 'vuetify';
+
   export default {
+    components: {
+      VRow,
+      VCol,
+    },
     name: 'AboutUs',
+    data() {
+      return {
+        techStack: techStackData,
+      };
+    },
   };
 </script>
 
