a1857e748e3d05cc30fe5506906b5812e6beacbd
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-04-01 21:15:23 +0300

    Update vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,3 +2,19 @@ import { createVuetify } from 'vuetify';
 import 'vuetify/dist/vuetify.css';
 
 export default createVuetify();
+
+module.exports = {
+  transpileDependencies: ['vuetify'],
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        options.compilerOptions = {
+          ...options.compilerOptions,
+          isCustomElement: tag => tag.startsWith('v-'),
+        };
+        return options;
+      });
+  },
+};
